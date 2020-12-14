fc5f84d9f6a690e46ae76da3edff286aed3c315a
jlier <joomli@hotmail.com>
2020-12-14 19:13:54 +0100

    slide up n down

--- public/js/budget.js
+++ public/js/budget.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
 
-	// var left = parseInt($("#left").val(), 10);
-	// var monthly = parseInt($("#monthly").val(), 10);
-	var dbleft = parseInt($("#left").val(), 10);
-	var dbmonthly = parseInt($("#monthly").val(), 10);
+	var leftDOM = $("#left");
+	var monthlyDOM = $("#monthly");
+
+	var dbleft = parseInt(leftDOM.val(), 10);
+	var dbmonthly = parseInt(monthlyDOM.val(), 10);
 
 	var date = new Date();
 	var today = date.getDate();
@@ -24,11 +25,10 @@ $(document).ready(() => {
 	var current_date_diff = Math.ceil((date - prev_salary) / 1000 / 3600 / 24)
 
 	var update = () => {
-		var left = parseInt($("#left").val(), 10);
-		var monthly = parseInt($("#monthly").val(), 10);
+		var left = parseInt(leftDOM.val(), 10);
+		var monthly = parseInt(monthlyDOM.val(), 10);
 
 		var res = (monthly / num_days * current_date_diff - monthly + left + left_add).toFixed(2);
-		console.log("Res 1:", res);
 		var altRes = ((left + left_add) / (num_days - current_date_diff + 1)).toFixed(2);
 
 		var stat = $("#statusText");
@@ -62,27 +62,6 @@ $(document).ready(() => {
 	}
 
 	update();
-	
-	// var res = (monthly / num_days * current_date_diff - monthly + left + left_add).toFixed(2);
-	// var altRes = ((left + left_add) / (num_days - current_date_diff + 1)).toFixed(2);
-
-	// var stat = $("#statusText");
-	// var altStat = $("#altStatusText");
-
-	// var prefix = "";
-
-	// if(res < 0) {
-	// 	stat.addClass('text-danger');
-
-	// 	var days_until_positive = Math.ceil((res * -1) / (monthly / num_days))
-	// 	$("#days").text(`${ days_until_positive } ${ days_until_positive === 1 ? 'dag' : 'dager'} bak skjema..`);
-	// }
-	// else {
-	// 	prefix = "+"
-	// }
-
-	// stat.text(`${ prefix }${ res } kr`);
-	// altStat.text(`${ altRes } kr/dag`);
 
 	$("#refresh").click(() => {
 		location.reload(true);
@@ -92,8 +71,23 @@ $(document).ready(() => {
 		location.redirect('/logout');
 	});
 
-	$("#left").change(() => {
-		update();
+	[leftDOM, monthlyDOM].forEach(element => {
+
+		element.change(() => {
+			update();
+		})
+
+		element.focus(() =>  {
+			var d = $("#display");
+			d.removeClass('align-items-center');
+			d.addClass('align-items-end');
+		})
+
+		element.focusout(() =>  {
+			var d = $("#display");
+			d.removeClass('align-items-end');
+			d.addClass('align-items-center');
+		})
 	})
 
 });
